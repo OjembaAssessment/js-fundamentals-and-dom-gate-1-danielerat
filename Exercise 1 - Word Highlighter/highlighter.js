053116a849f11caee4d6ebbe4197f2985b116773
danielerat <davidodo2015@gmail.com>
2023-07-24 12:28:59 +0200

    Fixing bug from our string sanitizer that did not take in accout the .

--- Exercise 1 - Word Highlighter/highlighter.js
+++ Exercise 1 - Word Highlighter/highlighter.js
@@ -1,6 +1,6 @@
 let myParagraph = document.querySelector("#myParagraph");
-//Remove All new Lines and return from our paragraph
-const sanitizedParagraph = myParagraph.textContent.replace(/[\r\n]+/g, "");
+//Remove All new Lines,return, and . from our paragraph
+const sanitizedParagraph = myParagraph.textContent.replace(/[\r\n.]+/g, "");
 const splitedParagraph = sanitizedParagraph.split(" ");
 
 const wordWithCout = splitedParagraph.reduce((acc, word) => {
