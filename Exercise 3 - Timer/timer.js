d3cad5937905e76b9e2a8efcad60475dddeffc22
danielerat <davidodo2015@gmail.com>
2023-07-24 10:05:30 +0200

    Refactoring Delegation and variable names

--- Exercise 3 - Timer/timer.js
+++ Exercise 3 - Timer/timer.js
@@ -1,29 +1,36 @@
-const secondButtons = document.querySelector("#timeButtons");
+const secondButtons = document.querySelector("#myTimer");
 const display = document.querySelector("[data-display]");
-let seconds = 0;
+const reset = document.querySelector("[data-button-reset]");
+// const start = document.querySelector("[]");
+let stopwatch = 0;
 
 secondButtons.addEventListener("click", (e) => {
+  //   Add Second
   if (e.target.matches("[data-time-add]")) {
     addOrRemoveSeconds(true);
-    console.log(seconds);
   }
+  // Remove Second
   if (e.target.matches("[data-time-remove]")) {
     addOrRemoveSeconds(false);
-    console.log(seconds);
+  }
+  //Reset  The counter to 0
+  if (e.target.matches("[data-button-reset]")) {
+    stopwatch = 0;
+    updateUi();
   }
 });
 // Function to add and remove seconds
 function addOrRemoveSeconds(state) {
   if (state) {
-    seconds += 1;
+    stopwatch += 1;
     updateUi();
   } else {
-    if (seconds > 0) seconds -= 1;
+    if (stopwatch > 0) stopwatch -= 1;
     updateUi();
   }
 }
 // Function to update the ui
 
 function updateUi() {
-  display.textContent = `${seconds}s`;
+  display.textContent = `${stopwatch}s`;
 }
