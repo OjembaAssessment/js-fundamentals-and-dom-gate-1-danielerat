4303fe3388700228f90d186fb778b53265eb0d12
danielerat <davidodo2015@gmail.com>
2023-07-24 11:26:58 +0200

    Reduce method to get the count of each word

--- Exercise 1 - Word Highlighter/highlighter.js
+++ Exercise 1 - Word Highlighter/highlighter.js
@@ -2,3 +2,9 @@ const myParagraph = document.querySelector("#myParagraph").textContent;
 //Remove All new Lines and return from our paragraph
 const sanitizedParagraph = myParagraph.replace(/[\r\n]+/g, "");
 const splitedParagraph = sanitizedParagraph.split(" ");
+
+const wordWithCout = splitedParagraph.reduce((acc, word) => {
+  let wordLowerCase = word.toLocaleLowerCase();
+  acc[wordLowerCase] = acc[wordLowerCase] + 1 || 1;
+  return acc;
+}, {});
