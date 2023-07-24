ee6d0395f91145130f9500ad894fa598887d2157
danielerat <davidodo2015@gmail.com>
2023-07-24 11:25:01 +0200

    Exercise 1 start Sanitized paragraph with no new line or returns

--- Exercise 1 - Word Highlighter/highlighter.js
+++ Exercise 1 - Word Highlighter/highlighter.js
@@ -1,1 +1,4 @@
-console.log("Exercise 1 - Word Highlighter");
+const myParagraph = document.querySelector("#myParagraph").textContent;
+//Remove All new Lines and return from our paragraph
+const sanitizedParagraph = myParagraph.replace(/[\r\n]+/g, "");
+const splitedParagraph = sanitizedParagraph.split(" ");
