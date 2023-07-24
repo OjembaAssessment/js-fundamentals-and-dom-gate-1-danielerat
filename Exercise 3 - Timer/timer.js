fae617a87bc33a6af44b1e9985b54b813e1cdaf8
danielerat <davidodo2015@gmail.com>
2023-07-24 09:56:36 +0200

    Function to Add-remove seconds and funciton to update the ui

--- Exercise 3 - Timer/timer.js
+++ Exercise 3 - Timer/timer.js
@@ -1,1 +1,29 @@
-console.log("Exercise 3 - Timer");
+const secondButtons = document.querySelector("#timeButtons");
+const display = document.querySelector("[data-display]");
+let seconds = 0;
+
+secondButtons.addEventListener("click", (e) => {
+  if (e.target.matches("[data-time-add]")) {
+    addOrRemoveSeconds(true);
+    console.log(seconds);
+  }
+  if (e.target.matches("[data-time-remove]")) {
+    addOrRemoveSeconds(false);
+    console.log(seconds);
+  }
+});
+// Function to add and remove seconds
+function addOrRemoveSeconds(state) {
+  if (state) {
+    seconds += 1;
+    updateUi();
+  } else {
+    if (seconds > 0) seconds -= 1;
+    updateUi();
+  }
+}
+// Function to update the ui
+
+function updateUi() {
+  display.textContent = `${seconds}s`;
+}
