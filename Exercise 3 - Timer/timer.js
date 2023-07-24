f9e2fa419ffb012dc1ffa079bec1ff3ca2b33ac4
danielerat <davidodo2015@gmail.com>
2023-07-24 10:17:09 +0200

    Mechanism to prevent the button from being clicked

--- Exercise 3 - Timer/timer.js
+++ Exercise 3 - Timer/timer.js
@@ -3,20 +3,26 @@ const display = document.querySelector("[data-display]");
 const reset = document.querySelector("[data-button-reset]");
 // const start = document.querySelector("[]");
 let stopwatch = 0;
-
+let secCounter = "";
+let stopWatchRunning = false;
 secondButtons.addEventListener("click", (e) => {
   //   Add Second
-  if (e.target.matches("[data-time-add]")) {
+  if (e.target.matches("[data-time-add]") && !stopWatchRunning) {
     addOrRemoveSeconds(true);
   }
   // Remove Second
-  if (e.target.matches("[data-time-remove]")) {
+  if (e.target.matches("[data-time-remove]") && !stopWatchRunning) {
     addOrRemoveSeconds(false);
   }
   //Reset  The counter to 0
-  if (e.target.matches("[data-button-reset]")) {
+  if (e.target.matches("[data-button-reset]") && !stopWatchRunning) {
     stopwatch = 0;
     updateUi();
+  }
+  //Start
+  if (e.target.matches("[data-button-start]")) {
+    stopWatchRunning = true;
+    secCounter = setInterval(coundDown, 1000);
   }
 });
 // Function to add and remove seconds
@@ -25,12 +31,24 @@ function addOrRemoveSeconds(state) {
     stopwatch += 1;
     updateUi();
   } else {
-    if (stopwatch > 0) stopwatch -= 1;
-    updateUi();
+    if (stopwatch > 0) {
+      stopwatch -= 1;
+      updateUi();
+    }
   }
 }
 // Function to update the ui
 
 function updateUi() {
   display.textContent = `${stopwatch}s`;
+}
+
+function coundDown() {
+  console.log("down");
+  if (stopwatch > 0) {
+    addOrRemoveSeconds(false);
+  } else {
+    clearInterval(secCounter);
+    stopWatchRunning = false; // You can run the timer again
+  }
 }
